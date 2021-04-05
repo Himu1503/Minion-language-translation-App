545503ba3cd1eb9cb23fa00e9c090bf3ff85da4a
Himu1503 <54092681+Himu1503@users.noreply.github.com>
2021-04-06 01:18:00 +0530

    App completed CSS left

--- app.js
+++ app.js
@@ -2,10 +2,27 @@ var btnTranslate= document.querySelector("#btn-translate")
 var inputTxt=document.querySelector("#input-Txt")
 var outputDiv=document.querySelector("#output")
 
+const url = "https://api.funtranslations.com/translate/minion.json"
 
 
-btnTranslate.addEventListener("click",clickEventHandler)
+
+
+function getTranslatedURL(text){
+    return url + "?" + "text=" + text
+}
 
 function clickEventHandler() {
- outputDiv.innerText= "asfsdsdfsds" + inputTxt.value
+
+    var inputText= inputTxt.value
+
+    
+
+fetch(getTranslatedURL(inputText))
+.then(response => response.json())
+.then(json =>{
+    var translatedText= json.contents.translated;
+    outputDiv.innerText =translatedText
+  })
 }
+  
+btnTranslate.addEventListener("click",clickEventHandler)
